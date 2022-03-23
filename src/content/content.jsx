30c4e47c7d950233e66f70ca23627821c05faeee
aveetron <aveechakra@gmail.com>
2022-03-23 19:52:21 +0600

    useState practice code added

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 
-const content = () => {
-    let name = 'Homepage';
+const Content = () => {
+
+    const [name, setName] = useState('mario');
+    let [age, setAge] = useState(20);
+
     const callMessage = () =>{
         console.log('avee chakraborty!');
     }
 
     const handleClick = () =>{
-        callMessage();
+        setName('avee');
+        setAge(27);
     };
 
     return(
         <div className="content">
-            <h3>{ name }</h3>
+            <h3>{ name } is {age} years old!</h3>
             <button onClick={handleClick}>Click Me</button>
         </div>
     )
 }
 
-export default content;
+export default Content;
