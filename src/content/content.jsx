ef3263dc526ee41246ebcccb7fffa05e1d7777f2
aveetron <aveechakra@gmail.com>
2022-03-24 12:39:41 +0600

    some css added in bloglist

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,7 +1,8 @@
+import './content.css';
 import { useState } from "react";
 
 const Content = () => {
-
+    const blog_title = "Blog List";
     const [blogs, setBlogs] = useState([
         {id:1, name:'avee chakraborty',content: 'uheiuhfifhiu ihfiu rhfihefiuhriu'},
         {id:2, name:'biplob chakraborty',content: 'uheiuhfifhiu ihfiu rhfihefiuhriu'},
@@ -9,10 +10,11 @@ const Content = () => {
 
     return(
         <div className="content">
+            <h2 className='blog-header'>{ blog_title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.name }</h2>
-                    <p>{ blog.content }</p>
+                    <h2 className="blog-title">{ blog.name }</h2>
+                    <p className="blog-content">{ blog.content }</p>
                 </div>
             ))}
         </div>
